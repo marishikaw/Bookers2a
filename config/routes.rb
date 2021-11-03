b4e06219d9ee6d722216642b41ca7ed957becd5a
marishikaw <90078966+marishikaw@users.noreply.github.com>
2021-11-03 09:57:11 +0000

    [Update] task8

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,12 @@
 Rails.application.routes.draw do
+  get 'chats/show'
   devise_for :users
   
   root to: 'homes#top'
   
   get '/home/about' => 'homes#about', as: 'about'
-  get '/search', to: 'searches#search'
+  get '/search' => 'searches#search'
+  get 'chat/:id' => 'chats#show', as: 'chat'
   
   resources :users,only: [:show,:index,:edit,:update] do
     resource :relationships, only: [:create, :destroy]
@@ -16,4 +18,6 @@ Rails.application.routes.draw do
     resource :favorites, only: [:create, :destroy]
     resources :book_comments, only: [:create, :destroy]
   end
+  
+  resources :chats, only: [:create]
 end
